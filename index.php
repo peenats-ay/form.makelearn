d347623253dfbbf18804aac906c0014cc69cd6d7
peenats-ay <65844340+peenats-ay@users.noreply.github.com>
2020-09-04 22:05:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 echo 'Contact Form';
 
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>contact form</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+  <h3>contact form</h3>
+</body>
+</html>
