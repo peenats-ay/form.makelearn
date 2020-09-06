95bcd454e77af67725a3d75994450634aa9e757c
peenats-ay <65844340+peenats-ay@users.noreply.github.com>
2020-09-06 21:05:30 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,59 @@
 <?php
+    session_start();
+    $mode = 'input';
+    if( isset($_POST['back']) && $_POST['back'] ){
 
-echo 'Contact Form';
-
+    } elseif( isset($_POST['confirm']) && $_POST['confirm'] ) {
+        $_SESSION['fullname'] = $_POST['fullname'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['message'] = $_POST['message'];
+        $mode = 'confirm';
+    } elseif( isset($_POST['send']) && $_POST['send'] ){
+        $message = "お問い合わせを受け付けました \r\n"
+                    . "お名前：" .$_SESSION['fullname'] . "\r\n" 
+                    . "Email：" .$_SESSION['email'] . "\r\n" 
+                    . "お問い合わせ内容：\r\n"
+                    . preg_replace("/\r\n|\r|\n", "\r\n", $_SESSION['message']);                
+        mail($_SESSION['email'], 'お問い合わせありがとうございます', $message);
+        mail('tenorlaaaaav@gmail.com', 'お問い合わせありがとうございます', $message);
+        $_SESSION = array();
+        $mode = 'send';
+    } else {
+        $_SESSION['fullname'] = "";
+        $_SESSION['email'] = "";
+        $_SESSION['message'] = "";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>contact form</title>
+    <title>お問い合わせフォーム</title>
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
 <body>
-  <h3>contact form</h3>
+    <?php if( $mode == 'input' ) : ?>
+        <form action="./index.php" method="post">
+            名前<input type="text" name="fullname" value="<?= $_SESSION['fullname'] ?>"><br>
+            Eメール<input type="email" name="email" value="<?= $_SESSION['email'] ?>"><br>
+            お問い合わせ内容<br>
+                <textarea cols="40" rows="8" name="message"><?= $_SESSION['message'] ?></textarea><br>
+            <input type="submit" name="confirm" value="確認">
+        </form>
+
+    <?php elseif( $mode == 'confirm' ) : ?>
+        <form action="./index.php" method="post">
+            名前　<?php echo $_SESSION['fullname'] ?><br>
+            Eメール　<?php echo $_SESSION['email'] ?><br>
+            お問い合わせ内容<br>　
+            <?php echo nl2br($_SESSION['message']) ?><br>
+            <input type="submit" name="back" value="戻る">
+            <input type="submit" name="send" value="送信">
+        </form>
+
+    <?php else : ?>
+        送信しました。お問い合わせありがとうございました。<br>
+    <?php endif ?>
+        
 </body>
 </html>
